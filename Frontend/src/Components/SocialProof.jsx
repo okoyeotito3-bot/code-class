5bb1117e10c838608bc75c23044047b0eb7f14e5
Otitochukwu Okoye <okoyeotito3@gmail.com>
2026-08-27 16:12:28 -0700

    build socialProof

---
+++ Frontend/src/Components/SocialProof.jsx
@@ -0,0 +1,10 @@
+export default function SocialProof(){
+    return(
+        <div className="bg-[#242E42] flex justify-between  p-4  md:px-20 md:py-10">  
+          <img src="metric-box-ActiveStudent.png" alt="metric-box-ActiveStudent" className="hidden md:block"/>
+          <img src="metric-box-CompletionRate.png" alt="metric-box-CompletionRate" />
+          <img src="metric-box-AvarageRate.png" alt="metric-box-AvarageRate" />
+          <img src="metric-box-LiveMentor.png" alt="metric-box-LiveMentor" />
+        </div>
+    )
+}
