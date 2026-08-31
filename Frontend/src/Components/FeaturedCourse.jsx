4a8c688a1b74d4b00e93e7959a85f9ae27a5e69b
Otitochukwu Okoye <okoyeotito3@gmail.com>
2026-08-30 23:05:18 -0700

    fix resposivness

--- Frontend/src/Components/FeaturedCourse.jsx
+++ Frontend/src/Components/FeaturedCourse.jsx
@@ -3,15 +3,27 @@ import Button from '../Utilities/Button'
 
 export default function FeatureCourse(){
     return(
-        <section className=" w-full flex flex-col gap-16 py-4 px-2 md:py-[80px] md:px-[120px] lg:py-[40px] lg:px-[120px]">
+        <section className=" w-full flex flex-col gap-4 py-4 px-2 md:py-[80px] md:px-[120px] lg:py-[40px] lg:px-[120px] md:gap-16  lg:gap-16 ">
             <header className="w-full flex flex-col gap-4 items-center">
                 <p className="text-[#FF4081] font-[Geist-Mono] font-semibold text-[11px]">PREMIUM COHORT</p>
-                <p className="font-geist font-extrabold text-2xl  text-[#F8FAFC] md:text-[36px] lg:text-[36px]">Our Best-Selling Syllabus</p>
+                <p className="font-geist font-extrabold text-2xl  text-[#F8FAFC] md:text-[36px] lg:text-[36px] ">Our Best-Selling Syllabus</p>
             </header>
 
             <div className="w-full flex flex-col md:flex-row lg:flex-row">
                 <div className="flex flex-col gap-[40px] p-12">
-                <img src="Frame.png" alt="Frame" />
+
+                <div className='flex flex-col w-full gap-4 '>
+                    <div className='flex gap-2 '>
+                        <span className='text-[#FFD600] font-semibold text-sm '>Javascript</span>
+                        <span className='text-[#5491FF] font-semibold text-sm'>12 weeks</span>
+                    </div>
+                    <h2 className='font-extrabold text-3xl text-[#F8FAFC] font-geist '>JavaScript Fundamentals to Advanced</h2>
+                    <p className='font-geist font-sm text-[#94A3B8]'>Become a master of vanilla JavaScript. Deeply understand the internals, 
+                        execute complex asynchronous tasks, 
+                        and learn how to package and optimize modern build files.
+                    </p>
+                </div>
+
                 <div className="flex items-center gap-4">
                     <img 
                      src="tutor.jpg" 
@@ -23,7 +35,7 @@ export default function FeatureCourse(){
                         <p className="font-normal font-[Geist-Mono] text-3 text-[#94A3B8]">Software Enginner</p>
                     </div>
                 </div>
-                <div className='w-full flex items-center justify-between md:pr-6 lg:pr-6'>
+                <div className='w-full flex items-center justify-between gap-2 md:pr-6 lg:pr-6'>
                     <div className='flex flex-col items-start gap-1 whitespace-nowrap'>
                       <p className='text-[#64748B] text-3 font-gesit font-semibold'>Price</p>  
                       <p className='text-[#00E676] font-[Geist-Mono] text-[32px]'>$199 <small className='text-[#94A3B8]'>/ total</small></p>
